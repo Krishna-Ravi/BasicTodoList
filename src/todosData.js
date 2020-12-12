3b6fd3e73cdf66bfe167c6aefdc1dfe49ceab698
Krishna R <58383978+Krishna-Ravi@users.noreply.github.com>
2020-12-12 13:13:12 +0530

    Add files via upload

---
+++ src/todosData.js
@@ -0,0 +1,34 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Aadhar Card",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Passport",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Pan Card",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Resumē",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Laptop",
+        completed: false
+    },
+    {
+        id: 6,
+        text: "Cell Phone",
+        completed: false
+    }
+]
+
+export default todosData
